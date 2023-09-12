9a19caaf747920a042d26d02977ea84b4b0e8cbe
CheHyeonyeong <gusdudco6@naver.com>
2023-09-12 10:58:30 +0900

    상속

---
+++ Week02_02_chy/src/Sports.java
@@ -0,0 +1,20 @@
+public class aSports {
+    String getName(){return "아직 결정되지 않음";}
+    int getPlayers(){return 0;}
+}
+
+class Soccer extends Sports{
+
+//경기 이름 : 축구, 경기자수 : 11
+    String getName(){return "축구";}
+    int getPlayers(){return 11;}
+}
+
+class SoccerTest{
+    public static void main(String[] args) {
+        Soccer obj = new Soccer();
+        System.out.println("경기이름 : "+obj.getName());
+        System.out.println("경기자수 : "+obj.getPlayers());
+    }
+
+}
---
+++ Week02_03_chy/src/Animal.java
@@ -0,0 +1,15 @@
+interface Animal {
+    void walk();
+    void fly();
+    void sing();
+}
+
+class bird{
+
+}
+
+public class AnimalTest{
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
