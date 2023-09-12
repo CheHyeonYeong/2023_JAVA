6d023c24d74ae5b707d8e025b189c902f72f73af
CheHyeonyeong <gusdudco6@naver.com>
2023-09-12 11:01:13 +0900

    override

--- Week02_02_chy/src/Sports.java
+++ Week02_02_chy/src/Sports.java
@@ -1,4 +1,4 @@
-public class aSports {
+public class Sports {
     String getName(){return "아직 결정되지 않음";}
     int getPlayers(){return 0;}
 }
@@ -6,7 +6,9 @@ public class aSports {
 class Soccer extends Sports{
 
 //경기 이름 : 축구, 경기자수 : 11
+    @Override
     String getName(){return "축구";}
+    @Override
     int getPlayers(){return 11;}
 }
 
