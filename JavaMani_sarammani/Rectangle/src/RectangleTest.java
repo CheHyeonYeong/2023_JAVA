69a2d3037c1cbb20cec22e8d74e7196eaeff3457
CheHyeonyeong <gusdudco6@naver.com>
2023-09-29 20:49:10 +0900

    생성자 사용

--- JavaMani_sarammani/Rectangle/src/RectangleTest.java
+++ JavaMani_sarammani/Rectangle/src/RectangleTest.java
@@ -12,19 +12,11 @@ public class RectangleTest {
         Rectangle rect1 = new Rectangle(0,0,10,5);
         RectangleTest.show(rect1);
 
-        Point k = new Point();
-        Point j = new Point();
-        k.setXY(1,1);
-        j.setXY(5,6);
-
-        Rectangle rect2 = new Rectangle(k,j);
+        Rectangle rect2 = new Rectangle(new Point(1,1), new Point(5,6));
         RectangleTest.show(rect2);
 
 
         System.out.println();
-
-        //Rectangle rect2 = new Rectangle(new Point(0,0) , new Point(5,10));
-        //RectangleTest.show(rect2);
     }
 
 }
