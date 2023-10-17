479732672936cc1c1bd6ecbf4d6d526837db03a1
CheHyeonyeong <gusdudco6@naver.com>
2023-10-17 10:50:53 +0900

    Stream

--- Week06_chy/Function/FunctionTest.java
+++ Week06_chy/Function/FunctionTest.java
@@ -5,7 +5,7 @@ import java.util.function.Function;
 public class FunctionTest {
     public static void main(String[] args){
         Function<Integer,Integer>f1 = i->i*4;
-        System.out.println(f1.apply(3)); //3*4=12 -> 12가 출력된다
+        System.out.println(f1.apply(3)); //3*4=12 -> 12가 출력된다----0-------------------
 
         Function<String,Integer>f2 = s->s.length();
         //매개변수와 반환값
