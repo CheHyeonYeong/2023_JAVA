1203906c54294848a08558d57d51eadf06d0216f
CheHyeonyeong <gusdudco6@naver.com>
2023-10-24 09:53:24 +0900

    file copy

---
+++ Week07_chy/CopyFile/CopyFile1.java
@@ -0,0 +1,19 @@
+package Week07_chy.CopyFile;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CopyFile1 {
+    public static void main(String[] args)  throws IOException {
+        try(FileReader in = new FileReader("test.txt");
+            FileWriter out = new FileWriter("Copy.txt")){
+            int c;
+            while ((c = in.read())!=-1){
+                out.write(c);
+            }
+        }
+
+    }
+
+}
