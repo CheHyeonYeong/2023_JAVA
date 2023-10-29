9ad92f187a3f63d5286fd70b3a0b83eb3ddeaadb
CheHyeonyeong <gusdudco6@naver.com>
2023-10-29 18:16:19 +0900

    java

--- Week04_chy/TriCircle.java
+++ Week04_chy/TriCircle.java
@@ -10,15 +10,15 @@ public class TriCircle extends JFrame {
         setSize(120,150);
         setVisible(true);
     }
-    class MyPanel extends JPanel{
-        public void paintComponent(Graphics g){
-            super.paintComponent(g);
-            g.setColor(Color.RED);
-            g.fillArc(10,10,100,100,90,120);
-            g.setColor(Color.green);
-            g.fillArc(10,10,100,100,210,120);
-            g.setColor(Color.YELLOW);
-            g.fillArc(10,10,100,100,330,120);
+            class MyPanel extends JPanel{
+                public void paintComponent(Graphics g){
+                    super.paintComponent(g);
+                    g.setColor(Color.RED);
+                    g.fillArc(10,10,100,100,90,120);
+                    g.setColor(Color.green);
+                    g.fillArc(10,10,100,100,210,120);
+                    g.setColor(Color.YELLOW);
+                    g.fillArc(10,10,100,100,330,120);
         }
     }
     public static void main(String [] args){
